37570689ffe3ac5535bb3d6d61f2cabc0f83eec4
erick1232456 <erickfernandorodriguez7@gmail.com>
2025-06-13 15:57:46 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,15 +3,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDbWx_JPmOOy3CAeRzwGwCbrIMo3XxJqew",
-  authDomain: "bdnoaql.firebaseapp.com",
-  projectId: "bdnoaql",
-  storageBucket: "bdnoaql.firebasestorage.app",
-  messagingSenderId: "637512184747",
-  appId: "1:637512184747:web:20becfaa04f48b351a98ee",
-  measurementId: "G-EXTTNGVH0J"
-};
-
+  
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
